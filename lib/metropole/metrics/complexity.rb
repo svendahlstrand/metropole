c6cfd339f1ca6232373d663b88524e9163296d31
Sven Dahlstrand <sven.dahlstrand@gmail.com>
2012-08-10 00:32:52 +0200

    Add complexity report.

--- lib/metropole/metrics/complexity.rb
+++ lib/metropole/metrics/complexity.rb
@@ -5,15 +5,17 @@ require 'flog'
 module Metropole
   module Metrics
     class Complexity
-      def initialize(ruby_file)
-        @ruby_file = ruby_file
-      end
+      attr_reader :total, :report
 
-      def total
+      def initialize(ruby_file)
         silence_warnings do
           flog = Flog.new continue: true, parser: RubyParser
-          flog.flog @ruby_file.path
-          flog.total.to_f.round(1)
+          flog.flog ruby_file.path
+          @total = flog.total.to_f.round(1)
+
+          report = StringIO.new '', 'r+:utf-8'
+          flog.report(report)
+          @report = report.string
         end
       end
     end
