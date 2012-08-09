01a4bd42ddde4d344dd8f692be4bd28e59ac9049
Sven Dahlstrand <sven.dahlstrand@gmail.com>
2012-08-10 00:02:13 +0200

    Make Reporter a class.

--- lib/metropole/reporter.rb
+++ lib/metropole/reporter.rb
@@ -4,27 +4,40 @@ require 'fileutils'
 require 'coderay'
 
 module Metropole
-  module Reporter
-    def self.run
+  class Reporter
+    def initialize
+      @all_files = RubyFile.all_in_current_dir
+      @all_files.sort! { |x, y| x.complexity.total <=> y.complexity.total}.reverse!
+    end
+
+    def run
+      create_overview
+      create_details_views
+      open_browser
+    end
+
+    private
+
+    def create_overview
       FileUtils.mkdir_p 'metropole'
 
-      all_files = RubyFile.all_in_current_dir
-      all_files.sort! { |x, y| x.complexity.total <=> y.complexity.total}.reverse!
-
       File.open('metropole/index.html', 'w:utf-8') do |f|
-        f.write View.new('index').render(all_files: all_files)
+        f.write View.new('index').render(all_files: @all_files)
       end
+    end
 
-      all_files.each do |ruby_file|
+    def create_details_views
+      @all_files.each do |ruby_file|
         FileUtils.mkdir_p ruby_file.html_dirname
 
         File.open(ruby_file.html_path, 'w:utf-8') do |f|
           f.write View.new('ruby_file').render(file: ruby_file, code: CodeRay.scan(ruby_file.content, :ruby).div(line_numbers: :table))
         end
       end
+    end
 
+    def open_browser
       exec 'open metropole/index.html'
     end
   end
 end
-
