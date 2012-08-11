a0d6b87b5c07e1771849f4aef02554f7acf59ca0
Sven Dahlstrand <sven.dahlstrand@gmail.com>
2012-08-12 01:44:36 +0200

    Sort by total score, complexity and duplication.

--- lib/metropole/reporter.rb
+++ lib/metropole/reporter.rb
@@ -7,7 +7,7 @@ module Metropole
   class Reporter
     def initialize
       @all_files = RubyFile.all_in_current_dir
-      @all_files.sort! { |x, y| x.complexity.total_score <=> y.complexity.total_score}.reverse!
+      @all_files.sort_by! { |file| file.complexity.total_score + file.duplication.total }.reverse!
     end
 
     def run
