20cd6bbaf79cf596e105b4cbac168e167d707f0d
Sven Dahlstrand <sven.dahlstrand@gmail.com>
2012-08-12 00:23:22 +0200

    Add duplication hotspots.

--- lib/metropole/metrics/duplication.rb
+++ lib/metropole/metrics/duplication.rb
@@ -1,26 +1,43 @@
 # encoding: utf-8
 
+require 'ostruct'
 require 'flay'
 
 module Metropole
   module Metrics
     class Duplication
-      attr_reader :total, :report
+      attr_reader :total, :hotspots
 
       def initialize(ruby_file)
         report = StringIO.new '', 'r+:utf-8'
         old_stdout, $stdout = $stdout, report
 
-        silence_warnings do
-          flay = Flay.new mass: 4, summary: true
-          flay.process ruby_file.path
-          @total = flay.total
+        flay = Flay.new mass: 15
+        flay.process ruby_file.path
+        @total = flay.total
 
-          flay.report(report)
-          @report = report.string
-        end
+        flay.report
+
+        @hotspots = get_hotspots(report.string)
       ensure
         $stdout = old_stdout
+      end
+
+      def get_hotspots(report)
+        hotspot = nil
+        hotspots = []
+
+        report.each_line do |line|
+          case line
+          when /^\d+\)/
+            hotspot = { heading: $'.strip, line_numbers: [] }
+            hotspots << OpenStruct.new(hotspot)
+          when /\w:(\d+)/
+            hotspot[:line_numbers] << $1.to_i
+          end
+        end
+
+        hotspots
       end
     end
   end
